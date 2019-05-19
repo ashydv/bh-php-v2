dcda515631ac152ba1d1a00b553bc69c7bf12f6c
ashydv <45968704+ashydv@users.noreply.github.com>
2019-05-19 22:05:56 +0530

    Add files via upload

---
+++ break.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-122x98.png" type="image/x-icon">
+  <meta name="description" content="">
+
+  <title>Bikerhood</title>
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/animatecss/animate.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/additional/css/additional.css" type="text/css">
+
+
+
+</head>
+<body>
+  <section class="menu cid-roDOZay7yl" once="menu" id="menu1-4">
+
+
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+
+                         <img src="assets/images/logo-122x98.png" alt="Mobirise" title="" style="height: 5.1rem;">
+
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="http://www.google.com" target="_blank">
+                        Bikerhood</a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-white display-4" href="http://www.google.com" target="_blank">Our Past Trips</a></li><li class="nav-item"><a class="nav-link link text-white display-4" href="http://www.google.com" target="_blank">
+
+                        Upcoming Trips</a></li><li class="nav-item"><a class="nav-link link text-white display-4" href="http://www.google.com" target="_blank">
+
+                        Rules to Follow</a></li><li class="nav-item"><a class="nav-link link text-white display-4" href="http://www.google.com">
+
+                        Contact Us
+                    </a></li></ul>
+
+        </div>
+    </nav>
+</section>
+
+<section class="engine"></section><section class="mbr-section content5 cid-roDPWKaTJN mbr-parallax-background" id="content5-7">
+
+
+
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(7, 59, 76);">
+    </div>
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="title col-12 col-md-8">
+                <h2 class="align-center mbr-bold mbr-white pb-3 mbr-fonts-style display-1"><br><br><span style="font-weight: normal;">Congratulations!!!</span></h2>
+                <h3 class="mbr-section-subtitle align-center mbr-light mbr-white pb-3 mbr-fonts-style display-5">
+                    <br>you just averted a disaster<br>Ride responsibly<br></h3>
+                <p class="mbr-text align-center mbr-white pb-3 mbr-fonts-style display-7">Always keep your<br>
+                Disaster Recovery Plans ready<br>
+                See you on next trip 🏍🏍🏍
+                </p>
+                <div class="mbr-section-btn align-center"><a class="btn btn-primary display-4" href="loop.php" target="_blank">Check full speed</a>
+                    <a class="btn btn-white-outline display-4" href="break.php" target="_blank">Emergency Break</a></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="cid-roDWcAy7nR mbr-reveal" id="footer5-e">
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="col-md-3">
+                <div class="media-wrap">
+
+                       <img src="assets/images/logo-192x154.png" title="">
+
+                </div>
+            </div>
+            <div class="col-md-9">
+                <p class="mbr-text align-right links mbr-fonts-style display-7">
+                    <a href="#" class="text-success">ABOUT</a> &nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="#" class="text-success">TERMS</a> &nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="#" class="text-success">CAREERS</a> &nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="#" class="text-success">CONTACT</a>
+                </p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-md-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-md-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">No copyrighted material, free to use for learning purpose.</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-list align-right">
+                        <div class="soc-item">
+                            <a href="https://twitter.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-twitter socicon" style="color: rgb(247, 237, 74); fill: rgb(247, 237, 74);"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon" style="color: rgb(247, 237, 74); fill: rgb(247, 237, 74);"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.youtube.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-youtube socicon" style="color: rgb(247, 237, 74); fill: rgb(247, 237, 74);"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://instagram.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon" style="color: rgb(247, 237, 74); fill: rgb(247, 237, 74);"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://plus.google.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-googleplus socicon" style="color: rgb(247, 237, 74); fill: rgb(247, 237, 74);"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.tumblr.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-tumblr socicon" style="color: rgb(247, 237, 74); fill: rgb(247, 237, 74);"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/viewportchecker/jquery.viewportchecker.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/formoid/formoid.min.js"></script>
+
+
+ <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i></a></div>
+    <input name="animation" type="hidden">
+  </body>
+</html>
